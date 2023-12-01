afe80da87f4c19c56e59d3cc5d71867b281d0bb2
Marie <marie.homtschenko@gmail.com>
2023-12-01 17:51:14 +0100

    updated modlist for pre 1

--- src/smp/modlist/Modlist.vue
+++ src/smp/modlist/Modlist.vue
@@ -10,7 +10,7 @@ import SearchList from '../../components/SearchList.vue';
             <h1>Server Modliste</h1>
             <p>Schau ob die Mods die du willst drin sind und wenn nicht dann <a href="https://www.curseforge.com/minecraft/search?page=1&gameVersion=1.12.2">such sie auf Curseforge</a>.<br>
             Diese Liste ist noch nicht entgültig.<br>
-            Lade die erste Testversion des Modpacks hier herunter <a href="https://drive.google.com/file/d/1hs4AWajWhDR6H43uDgZtSfurrjO9g1QC/view?usp=sharing">sexpack-alpha-1.zip</a>.</p>
+            Lade die erste Testversion des Modpacks hier herunter <a href="https://drive.google.com/file/d/13LSOkMDgVIk4DhwDw2km2exzpc6Iu5lB/view?usp=sharing">sexpack-pre-1.zip</a>.</p>
             <SearchList />
         </div>
         <p style="position: relative; text-align: center; color: gray">Powered by Github Pages<br>© 2023 Die Coolen Allgemeinen Ficker</p>
